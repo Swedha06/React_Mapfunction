8a94405d61cdb798d64a5525311932acbe1e8185
Swedha06 <swedhamal12@gmail.com>
2022-12-11 10:43:18 -0800

    Mapfunction project added

---
+++ src/PlanetList.js
@@ -0,0 +1,15 @@
+import React from'react';
+import Planet from './Planet.css'
+
+function PlanetList()
+{
+    const planets=['Mars','Venus','Jupiter','Earth','Saturn','Neptune'];
+    return (
+            <div className="bottom">
+            {
+                planets.map(planet => <table><tr><td>{planet}</td></tr></table>)
+            }
+        </div>     
+    )
+}
+export default PlanetList
